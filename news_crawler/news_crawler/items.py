09f4225db5034c7e2a6d53b1b2c8132a4600d3a4
Gabor Berta <bertagabor99@gmail.com>
2023-10-01 17:36:31 +0200

    items: title and content added

--- news_crawler/news_crawler/items.py
+++ news_crawler/news_crawler/items.py
@@ -8,5 +8,5 @@ import scrapy
 
 class NewsCrawlerItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    content = scrapy.Field()
