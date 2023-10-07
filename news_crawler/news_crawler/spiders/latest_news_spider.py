fa189f1c5b867435a98f1c7ca3e3c709654cce9c
Gabor Berta <bertagabor99@gmail.com>
2023-10-07 09:50:59 +0200

    Renamed news spider to latest_news_spider

---
+++ news_crawler/news_crawler/spiders/latest_news_spider.py
@@ -0,0 +1,29 @@
+import scrapy
+import psycopg2
+from ..items import NewsCrawlerItem
+
+class LatestNewsSpider(scrapy.Spider):
+    name = 'latestnewsspider'
+    start_urls = ['https://cybersecuritynews.com/']
+
+    def parse(self, response):
+        # Loop through the article links and follow them
+        for article_link in response.xpath('/html/body/div[6]/div[2]/div/div/div/div[3]/div/div[1]//a/@href').extract(): # loop for each latest article link TODO
+            yield response.follow(article_link, callback=self.parse_article)
+
+    def parse_article(self, response):
+        # Extract article data
+        article = NewsCrawlerItem()
+        article['id'] = response.url.split('/')[-2]  # Article ID / string! from URL - unique?
+        article['title'] = response.xpath('/html/head/title/text()').get()
+        article['body'] = response.xpath('/html/body').get()
+
+        
+        print("ARTICLE")
+        print(article['id'])
+        print(article['title'])
+        print(article['body'])
+        
+        # Save article data to PostgreSQL # TODO
+        yield article
+
