10327a9dc1cb5e4169a712fc46071f2b74d5985f
Gabor Berta <bertagabor99@gmail.com>
2023-10-18 22:15:04 +0200

    example_queries.sql: Added restart for serials

--- news_crawler/sql/example_queries.sql
+++ news_crawler/sql/example_queries.sql
@@ -1,3 +1,5 @@
 -- TRUNCATE news_crawler.article CASCADE; -- deletes all data
+-- ALTER SEQUENCE news_crawler."article_ID_seq" RESTART WITH 1;
+-- ALTER SEQUENCE news_crawler."common_ID_seq" RESTART WITH 1;
 SELECT * FROM news_crawler.article;
 SELECT * FROM news_crawler."common";
