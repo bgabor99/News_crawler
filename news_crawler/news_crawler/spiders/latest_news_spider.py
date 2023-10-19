fad96fe3590295a51aa2db9d1aae4be779a551ff
Gabor Berta <bertagabor99@gmail.com>
2023-10-19 08:11:57 +0200

    latest_news_spider.py: Logging spider name

--- news_crawler/news_crawler/spiders/latest_news_spider.py
+++ news_crawler/news_crawler/spiders/latest_news_spider.py
@@ -1,5 +1,6 @@
 import scrapy
 from ..items import NewsCrwalerItem
+import logging
 
 class LatestNewsSpider(scrapy.Spider):
     name = 'latestnewsspider'
@@ -7,6 +8,8 @@ class LatestNewsSpider(scrapy.Spider):
     start_urls = ['https://cybersecuritynews.com/']
 
     def parse(self, response):
+        logger = logging.getLogger(f'Spider - {self.name}')
+        logger.info('Spider parse started')
         # Loop through the article links and follow them
         for article_link in response.xpath('/html/body/div[6]/div[2]/div/div/div/div[3]/div/div[1]//a/@href').extract():
             yield response.follow(article_link, callback=self.parse_article)
