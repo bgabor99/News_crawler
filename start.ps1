0e6a1ecaccceae93a7401df34d9404eee68a9fb2
Gabor Berta <bertagabor99@gmail.com>
2023-10-19 08:10:03 +0200

    start.ps1: log level default now

--- start.ps1
+++ start.ps1
@@ -2,7 +2,7 @@ python3 -m venv env
 .\env\Scripts\activate
 pip3 install -r requirements.txt
 cd news_crawler\news_crawler\spiders\
-scrapy crawl latestnewsspider -L ERROR
-scrapy crawl threatnewsspider -L ERROR
+scrapy crawl latestnewsspider
+scrapy crawl threatnewsspider
 cd ..\..\..
 deactivate
