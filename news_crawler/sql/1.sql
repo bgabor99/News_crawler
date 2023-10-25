d5b23a24f5708592d7af0c26ca1dc85add1e0558
Gabor Berta <bertagabor99@gmail.com>
2023-10-26 00:23:46 +0200

    1.sql: Reworked to create schema in postgres database not in new database and creat admin_spider user after table creations

--- news_crawler/sql/1.sql
+++ news_crawler/sql/1.sql
@@ -1,17 +1,8 @@
 -- -- DROP DATABASE IF EXISTS news_crawler;
--- CREATE DATABASE news_crawler;
+-- CREATE DATABASE news_crawler; # not creating new one for this, using postgres database with new schema
+
 -- DROP SCHEMA IF EXISTS news_crawler CASCADE;
 CREATE SCHEMA IF NOT EXISTS news_crawler; --in news_crawler database
-
--- REASSIGN OWNED BY admin_spider TO postgres;  -- or some other trusted role
--- DROP OWNED BY admin_spider;
--- DROP USER IF EXISTS admin_spider;
--- CREATE USER admin_spider WITH ENCRYPTED PASSWORD 'password'; -- set your password
--- ALTER USER admin_spider WITH PASSWORD 'password'; -- password can be changaed with this query
--- GRANT ALL PRIVILEGES ON DATABASE "news_crawler" to admin_spider;
--- GRANT ALL PRIVILEGES ON SCHEMA "news_crawler" to admin_spider;
--- GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA "news_crawler" to admin_spider; -- if tables are already exist
--- GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA news_crawler to admin_spider;
 
 -- DROP TABLE IF EXISTS news_crawler.article CASCADE;
 CREATE TABLE news_crawler.article (
@@ -48,3 +39,13 @@ ALTER TABLE news_crawler."common" ADD CONSTRAINT "common_FK" FOREIGN KEY ("Artic
 REFERENCES news_crawler.article ("Article_ID") MATCH FULL
 ON DELETE NO ACTION ON UPDATE NO ACTION;
 -- ddl-end --
+
+-- REASSIGN OWNED BY admin_spider TO postgres;  -- or some other trusted role
+-- DROP OWNED BY admin_spider;
+DROP USER IF EXISTS admin_spider;
+CREATE USER admin_spider WITH ENCRYPTED PASSWORD 'password'; -- set your password
+-- ALTER USER admin_spider WITH PASSWORD 'password'; -- password can be changaed with this query
+GRANT ALL PRIVILEGES ON DATABASE "postgres" to admin_spider;
+GRANT ALL PRIVILEGES ON SCHEMA "news_crawler" to admin_spider;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA "news_crawler" to admin_spider; -- if tables are already exist
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA news_crawler to admin_spider;
