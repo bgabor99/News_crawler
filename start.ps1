37eaa9ca265b3faaa2abd20296e4cdfd1e3361f7
Gabor Berta <bertagabor99@gmail.com>
2023-11-26 15:18:10 +0100

    start.ps1: Run inputpagespider only for now

--- start.ps1
+++ start.ps1
@@ -2,7 +2,8 @@ python3 -m venv env # Developed in python 3.11
 .\env\Scripts\activate
 pip3 install -r requirements.txt
 cd news_crawler\news_crawler\spiders\
-scrapy crawl cybersecuritynewsspider
-scrapy crawl thehackernewsspider
+# scrapy crawl cybersecuritynewsspider
+# scrapy crawl thehackernewsspider
+scrapy crawl inputpagespider
 cd ..\..\..
 deactivate
