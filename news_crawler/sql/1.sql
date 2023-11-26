7a2e1b1f7865d1c58e06f767bc3661564f714974
Gabor Berta <bertagabor99@gmail.com>
2023-11-26 15:18:55 +0100

    1.sql: Changed the two table names

--- news_crawler/sql/1.sql
+++ news_crawler/sql/1.sql
@@ -1,42 +1,39 @@
--- -- DROP DATABASE IF EXISTS news_crawler;
--- CREATE DATABASE news_crawler; # not creating new one for this, using postgres database with new schema
-
 -- DROP SCHEMA IF EXISTS news_crawler CASCADE;
-CREATE SCHEMA IF NOT EXISTS news_crawler; --in news_crawler database
+CREATE SCHEMA IF NOT EXISTS news_crawler; --in postgres database
 
--- DROP TABLE IF EXISTS news_crawler.article CASCADE;
-CREATE TABLE news_crawler.article (
+-- DROP TABLE IF EXISTS news_crawler.common CASCADE;
+CREATE TABLE news_crawler.common (
 	"ID" serial NOT NULL,
 	"Domain" text NOT NULL,
-	"Article_ID" text NOT NULL,
+	"Page_ID" text NOT NULL,
 	"Processed_Date" timestamp NOT NULL,
 	CONSTRAINT article_pk PRIMARY KEY ("ID"),
-	CONSTRAINT "Unique_Article_ID" UNIQUE ("Article_ID")
+	CONSTRAINT "Unique_Page_ID" UNIQUE ("Page_ID")
 );
 -- ddl-end --
--- ALTER TABLE news_crawler.article OWNER TO postgres;
+-- ALTER TABLE news_crawler.common OWNER TO postgres;
 -- ddl-end --
 
--- DROP TABLE IF EXISTS news_crawler."common" CASCADE;
-CREATE TABLE news_crawler."common" (
+-- DROP TABLE IF EXISTS news_crawler."article" CASCADE;
+CREATE TABLE news_crawler."article" (
 	"ID" serial NOT NULL,
-	"Article_ID" text NOT NULL,
+	"Page_ID" text NOT NULL,
 	"Title" text,
 	"Body" text,
 	"Content" text,
 	"Author" text,
 	"Date" text,
-	CONSTRAINT "Latest_news_pk" PRIMARY KEY ("ID")
+	CONSTRAINT "Article_pk" PRIMARY KEY ("ID")
 );
 -- ddl-end --
--- ALTER TABLE news_crawler."common" OWNER TO postgres;
+-- ALTER TABLE news_crawler."article" OWNER TO postgres;
 -- ddl-end --
 
 -- ddl-end --
--- object: "common_FK" | type: CONSTRAINT --
--- ALTER TABLE news_crawler."common" DROP CONSTRAINT IF EXISTS "common_FK" CASCADE;
-ALTER TABLE news_crawler."common" ADD CONSTRAINT "common_FK" FOREIGN KEY ("Article_ID")
-REFERENCES news_crawler.article ("Article_ID") MATCH FULL
+-- object: "Article_FK" | type: CONSTRAINT --
+-- ALTER TABLE news_crawler."article" DROP CONSTRAINT IF EXISTS "Article_FK" CASCADE;
+ALTER TABLE news_crawler."article" ADD CONSTRAINT "Article_FK" FOREIGN KEY ("Page_ID")
+REFERENCES news_crawler.common ("Page_ID") MATCH FULL
 ON DELETE NO ACTION ON UPDATE NO ACTION;
 -- ddl-end --
 
