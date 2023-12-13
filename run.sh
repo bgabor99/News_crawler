d4ff6e5396487067050aeea01c47bf955491e428
Gabor Berta <bertagabor99@gmail.com>
2023-12-13 23:26:22 +0100

    Run.sh: sleep in while true added for schedule crawling again

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-cd news_crawler/news_crawler/spiders/
-scrapy crawl cybersecuritynewsspider
-scrapy crawl thehackernewsspider
+
+while true; do
+
+    # crawl
+    cd news_crawler/news_crawler/spiders/
+    scrapy crawl cybersecuritynewsspider
+    scrapy crawl thehackernewsspider
+
+    # Sleep for 30 minutes
+    sleep 1800
+done
